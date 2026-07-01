cab9bb323562713b252a99d45b2a935b7fa3e4be
Paulo Sousa <psousa@alticelabs.com>
2026-07-01 08:00:36 +0000

    Update Go Fundamentals: Upgrade Go version, restructure devcontainer, and remove outdated examples

---
+++ training/Go_Fundamentals/01.05 - Stdout Values/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+	fmt.Println("Hello " + "World")
+	fmt.Println("1 + 1 = ", 1+1)
+	fmt.Println("1.1 + 2.2 = ", 1.1+2.2)
+	fmt.Println(true)
+}
